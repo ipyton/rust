039f919a3674464f83203c3d25f3a6787ab41e76
chen <1278341834@qq.com>
2022-09-21 00:28:51 +0800

    rust

---
+++ src/hello.rs
@@ -0,0 +1,9 @@
+fn hello() {
+    fn main(){
+        const a: i32 = 10;
+        println!("I am {}",a);
+
+
+    }
+
+}
---
+++ src/rectangle.rs
@@ -0,0 +1,31 @@
+use rand::Rng;
+use std::io;
+use std::cmp::Ordering;
+fn rec() {
+    println!("GuessNumber");
+    let secret_number = rand::thread_rng().gen_range(1,101);
+    println!("The secret number is:{}",secret_number);
+    loop{
+        println!("input your guess");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        println!("you input {}!",guess);
+        let guess:u32=match guess.trim().parse(){//类型转>换 同时复用了变量名。
+            Ok(num) => num,
+            Err(_) => {
+                println!("your input is invalid");
+                continue;
+            }};
+        println!("input your guess");
+        println!("helloworld");
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("TooBig"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            },
+        };
+    }
+}
+
